2cac3909329bb131eb469cd3961189c30bf4ad8f
matt-edgedesign <matt@steve-edge.com>
2021-10-11 14:13:24 +0100

    Updated parallax TL

--- src/script.js
+++ src/script.js
@@ -10,12 +10,58 @@ timeline.from(".square-1", {
   y: -1000, //direction and amount
   duration: 2,
 }, '-=2')
-.to(".dynamic-text .text", {
+.from(".square-3", {
+  y: 1000, //direction and amount
+  duration: 2,
+})
+.from(".square-4", {
+  y: -1000, //direction and amount
+  duration: 2,
+}, '-=2')
+.from(".square-5", {
+  y: 1000, //direction and amount
+  duration: 2,
+})
+.from(".square-6", {
+  y: -1000, //direction and amount
+  duration: 2,
+}, '-=2')
+.to(".dynamic-text-1 .text", {
   autoAlpha: 1,
   duration: 0.5,
   y: 0,
   ease: Sine.easeInOut,
-}, '-=1.5');
+}, '-=6')
+.to(".dynamic-text-1 .text", {
+  autoAlpha: 0,
+  duration: 0.5,
+  y: -40,
+  ease: Sine.easeInOut,
+}, '-=5')
+.to(".dynamic-text-2 .text", {
+  autoAlpha: 1,
+  duration: 0.5,
+  y: 0,
+  ease: Sine.easeInOut,
+}, '-=4')
+.to(".dynamic-text-2 .text", {
+  autoAlpha: 0,
+  duration: 0.5,
+  y: -40,
+  ease: Sine.easeInOut,
+}, '-=3')
+.to(".dynamic-text-3 .text", {
+  autoAlpha: 1,
+  duration: 0.5,
+  y: 0,
+  ease: Sine.easeInOut,
+}, '-=2')
+.to(".dynamic-text-3 .text", {
+  autoAlpha: 0,
+  duration: 0.5,
+  y: -40,
+  ease: Sine.easeInOut,
+}, '-=1');
 
 ScrollTrigger.create({
   trigger: ".middle",
@@ -26,7 +72,7 @@ ScrollTrigger.create({
   pin: true,
   scrub: 1,
   start: "top top",
-  end: () => "+=" + window.innerHeight*(4),
+  end: () => "+=" + window.innerHeight*(8),
 });
 
 // gsap.from(".square-1", {
