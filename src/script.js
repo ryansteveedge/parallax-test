00aff72d49342f08e5e0410439bd5dafd09427f1
matt-edgedesign <matt@steve-edge.com>
2021-10-11 12:14:42 +0100

    Updated parllax timeline

--- src/script.js
+++ src/script.js
@@ -1,24 +1,51 @@
-// gsap.to(".square", {
-//   // x: 1000, //direction and amount
-//   duration: 8, //how long
+gsap.registerPlugin(ScrollTrigger);
+
+var timeline = gsap.timeline();
+
+timeline.from(".square-1", {
+  y: 1000, //direction and amount
+  duration: 2,
+})
+.from(".square-2", {
+  y: -1000, //direction and amount
+  duration: 2,
+}, '-=2')
+.to(".dynamic-text .text", {
+  autoAlpha: 1,
+  duration: 0.5,
+  y: 0,
+  ease: Sine.easeInOut,
+}, '-=1.5');
+
+ScrollTrigger.create({
+  trigger: ".middle",
+  animation: timeline,
+  // Uncomment these to see how they affect the ScrollTrigger
+  markers: true,
+  // toggleClass: "active",
+  pin: true,
+  scrub: 1,
+  start: "top top",
+  end: () => "+=" + window.innerHeight*(4),
+});
+
+// gsap.from(".square-1", {
+//    y: 1000, //direction and amount
+//    duration: 2,
 //   scrollTrigger: {
-//      trigger: ".div2", //Starts when this element comes in
-//      start: "top 80%", //when top of trigger meets 80 of viewport
-//      end: "bottom bottom",
-//      scrub: true, //change to numerical value for smoother scrolling
-//      // pin: ".square", //pin can be changed to trigger or an element
-//      // pinSpacing: false,
-//      toggleActions: "play none reverse none",
-//      //              onEnter onLeave onEnterBack onLeaveBack
-//      // end: () => "+=" + document.querySelector(".square").offsetHeight, //height of element
-//      markers: true,
-//      // toggleClass: "red" //Once Scroll trigger starts this class is added to the square
-//  }
+//     trigger: ".middle",
+//     scrub: true,
+//     pin: true,
+//     pinSpacing: true,
+//     start: "top top",
+//      end: () => "+=" + window.innerHeight*(4),
+//     markers: true,
+
+//   },
 // });
 
-// --- ORANGE PANEL ---
-// gsap.from(".text", {
-//    // y: 1000, //direction and amount
+// gsap.from(".square-2", {
+//    y: -1000, //direction and amount
 //    duration: 8,
 //   scrollTrigger: {
 //     trigger: ".middle",
@@ -26,55 +53,8 @@
 //     pin: true,
 //     pinSpacing: true,
 //     start: "top top",
-//     end: "+=100%",
+//      end: () => "+=" + window.innerHeight*(4),
 //     markers: true,
+
 //   },
 // });
-
-gsap.from(".square-1", {
-   y: 1000, //direction and amount
-   duration: 8,
-  scrollTrigger: {
-    trigger: ".middle",
-    scrub: true,
-    pin: true,
-    pinSpacing: true,
-    start: "top top",
-     end: () => "+=" + window.innerHeight*(4),
-    markers: true,
-
-  },
-});
-
-// var circles = gsap.utils.toArray('.square-2')
-//
-// circles.forEach((circle, i) => {
-//
-//   var circleFade = gsap.fromTo(circle, { autoAlpha: 0 }, { duration: 0.1, autoAlpha: 1 }, 0)
-//
-//   ScrollTrigger.create({
-//      trigger: ".middle",
-//    scrub: true,
-//    pin: true,
-//    pinSpacing: true,
-//    start: "top top",
-//    end: () => "+=" + window.innerHeight,
-//    markers: true,
-//   })
-//
-// });
-
-gsap.from(".square-2", {
-   y: -1000, //direction and amount
-   duration: 8,
-  scrollTrigger: {
-    trigger: ".middle",
-    scrub: true,
-    pin: true,
-    pinSpacing: true,
-    start: "top top",
-     end: () => "+=" + window.innerHeight*(4),
-    markers: true,
-
-  },
-});
